394d5cb1124b87ea5e59d480c38cde0bb4610464
anuj solania <anujsolania0507@gmail.com>
2025-01-02 01:01:07 +0530

    useMemo assignments

---
+++ week6/1-use-memo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Assignment1 } from './assignments/Assignment1'
+import { Assignment2 } from './assignments/Assignment2'
+import { Assignment3 } from './assignments/Assignment3'
+
+function App() {
+
+  return (
+    <>
+      {/* <Assignment1></Assignment1>
+      <Assignment2></Assignment2> */}
+      <Assignment3></Assignment3>
+    </>
+  )
+}
+
+export default App
