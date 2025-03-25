ad2d3f33165c788ce40324aee7b5534e5c4cdcb4
anuj solania <anujsolania0507@gmail.com>
2025-03-26 00:18:43 +0530

    types,enums,generics

--- week9/Typescript/dist/a.js
+++ week9/Typescript/dist/a.js
@@ -1,37 +1,13 @@
 "use strict";
-let x = 1;
-console.log(x);
-function greet(firstName) {
-    console.log("Hello " + firstName);
+var role;
+(function (role) {
+    role[role["admin"] = 0] = "admin";
+    role[role["user"] = 1] = "user";
+    role[role["guest"] = 2] = "guest";
+})(role || (role = {}));
+console.log(role.admin);
+function print(x) {
+    return x[1];
 }
-function sum(a, b) {
-    return a + b;
-}
-function teenager(age) {
-    if (age > 18) {
-        return true;
-    }
-    return false;
-}
-function fn(fn1) {
-    setTimeout(fn1, 1000);
-}
-function fn1(x) {
-    console.log("i am " + x);
-}
-fn(() => fn1("funtion 1"));
-greet("Anuj Solania");
-console.log(sum(2, 3));
-console.log(teenager(18));
-const user = {
-    firstname: "Anuj",
-    lastname: "Solania",
-    age: 20
-};
-function isLegal(user) {
-    if (user.age > 18) {
-        return true + "i.e LEGAL";
-    }
-    return false;
-}
-console.log(isLegal(user));
+const y = print(["anuj", 7]);
+print(10);
--- week9/Typescript/src/a.ts
+++ week9/Typescript/src/a.ts
@@ -1,48 +1,97 @@
-let x : number = 1
-// x = "hello"
-console.log(x)
+// let x : number = 1
+// // x = "hello"
+// console.log(x)
 
-function greet(firstName: string) {
-    console.log("Hello " + firstName);
+// function greet(firstName: string) {
+//     console.log("Hello " + firstName);
+// }
+
+// function sum(a: number,b: number): number {
+//     return a + b;
+// }
+
+// function teenager(age: number): boolean {
+//     if (age > 18) { return true }
+//     return false
+// }
+
+// function fn(fn1: (x: string)=>void) {
+//     setTimeout(fn1,1000);
+// }
+// function fn1(x: string) {
+//     console.log("i am "+ x)
+// }
+
+// fn(() => fn1("funtion 1") )
+// greet("Anuj Solania");
+// console.log(sum(2,3))
+// console.log(teenager(18))
+
+
+// const user = {
+//     firstname: "Anuj",
+//     lastname: "Solania",
+//     age: 20
+// }
+
+// interface User {
+//     firstname: string
+//     lastname: string
+//     age: number
+// } 
+
+// function isLegal(user: User) {
+//     if (user.age>18) {
+//         return true + " i.e LEGAL"
+//     } return false
+// }
+// console.log(isLegal(user))
+
+// type value = number | string
+
+// function print(value: value ) {
+//     console.log(value)
+// }
+// print(23)
+// print("anuj")
+
+// type student = {
+//     name: string,
+//     age: number
+// }
+// type teacher = {
+//     salary: number
+// }
+// type both = student | teacher
+
+// let user: both = {
+//     salary:6346834
+// }
+
+// type arr = number[]
+// let numbers: arr = [2,5,56,43]
+
+//ENUMS
+
+enum role  {
+    admin,
+    user,
+    guest
+}
+console.log(role.admin)
+
+//GENRICS 
+function print<T>(x: T): T {
+    return x
 }
 
-function sum(a: number,b: number): number {
-    return a + b;
-}
-
-function teenager(age: number): boolean {
-    if (age > 18) { return true }
-    return false
-}
-
-function fn(fn1: (x: string)=>void) {
-    setTimeout(fn1,1000);
-}
-function fn1(x: string) {
-    console.log("i am "+ x)
-}
-
-fn(() => fn1("funtion 1") )
-greet("Anuj Solania");
-console.log(sum(2,3))
-console.log(teenager(18))
+const y  = print<string>("anuj")
+const w = print<number>(10)
 
 
-const user = {
-    firstname: "Anuj",
-    lastname: "Solania",
-    age: 20
-}
 
-interface User {
-    firstname: string
-    lastname: string
-    age: number
-} 
 
-function isLegal(user: User) {
-    if (user.age>18) {
-        return true + " i.e LEGAL"
-    } return false
-}
-console.log(isLegal(user))
+
+
+
+
