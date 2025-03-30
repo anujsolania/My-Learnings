379d24d1933e4fc25b52756676c9aa301bdb7fe5
anuj solania <anujsolania0507@gmail.com>
2025-03-31 04:54:40 +0530

    prisma-ORM SQL queries

--- week10/Prisma-ORM/src/index.ts
+++ week10/Prisma-ORM/src/index.ts
@@ -1,1 +1,40 @@
-import 'dotenv/config';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(username: string,email: string,password:string) {
+    const res = await prisma.users.create({
+        data: {
+            username,
+            email,
+            password
+        }
+    })
+    console.log(res)
+}
+
+interface update {
+    username: string
+    password: string
+}
+
+async function updateUser(email: string,{username,password}: update) {
+    const res = await prisma.users.update({
+        where: {email},
+        data: {
+            username,
+            password
+        }
+    })
+    console.log(res)
+}
+
+async function getUser(username: string) {
+    const res = await prisma.users.findFirst({
+        where: {username}
+    })
+    console.log(res)
+}
+
+// insertUser("anuj","anuj@gmail.com","haleluya")
+getUser("jaiii")
