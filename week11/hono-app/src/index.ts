999a86e1753630bd37c049836152a515eb0cf114
anuj solania <anujsolania0507@gmail.com>
2025-04-03 01:05:17 +0530

    testing the routes

--- week11/hono-app/src/index.ts
+++ week11/hono-app/src/index.ts
@@ -2,6 +2,25 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.use(async (c, next) => {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  } else {
+    return c.text("You dont have acces");
+  }
+})
+
+app.get('/', async (c) => {
+  const body = await c.req.parseBody()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.json({msg: "as"})
+})
+
+
 app.post('/', async (c) => {
   const body = await c.req.json()
   console.log(body);
