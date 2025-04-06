cd77ae306a3fc172dc455609f5aef26383e7b14d
anuj solania <anujsolania0507@gmail.com>
2025-04-06 21:59:19 +0530

    Partial,Records,Maps,Exclude etc

--- week12/AdvanceTsAPIs/dist/index.js
+++ week12/AdvanceTsAPIs/dist/index.js
@@ -13,3 +13,28 @@ const user2 = {
     email: "bablu@gmail.com"
 };
 userprofile(user1);
+const user3 = {
+    name: "dabluuuu"
+};
+function printname(user) {
+    console.log(user.name);
+}
+printname(user3);
+const config = {
+    endpoint: 'https://api.example.com',
+    apiKey: 'abcdef123456',
+};
+const user = {
+    "a": { id: "vhsv", name: "anuj" },
+    "b": { id: "vhfev", name: "anujuwebf" }
+};
+//MAPS 
+const userMap = new Map();
+userMap.set("c", { id: "hello", name: "bjdhvbsdjs" });
+userMap.set("d", { id: "heldwvdlo", name: "bjdhvbdsvdsvssdjs" });
+console.log(userMap.get("c"));
+function eventfn(event) {
+    console.log(event);
+}
+// eventfn("mousemove")  //Not possible
+eventfn("click");
--- week12/AdvanceTsAPIs/src/index.ts
+++ week12/AdvanceTsAPIs/src/index.ts
@@ -21,3 +21,55 @@ const user2 = {
     email: "bablu@gmail.com"
 }
 userprofile(user1)
+
+//PARTIAL 
+type optional = Partial<User>
+const user3 = {
+    name: "dabluuuu"
+}
+function printname(user: optional) {
+    console.log(user.name)
+}
+printname(user3)
+
+//READ-ONLY
+interface Config {
+    readonly endpoint: string;
+    readonly apiKey: string;
+  }
+  
+  const config: Config = {
+    endpoint: 'https://api.example.com',
+    apiKey: 'abcdef123456',
+  };
+  
+//config.apiKey = 'newkey' //not possible
+
+//RECORDS 
+interface User1 {
+    id : string,
+    name: string
+}
+const user: Record<string,User1> = {
+    "a": {id: "vhsv", name: "anuj"},
+    "b": {id: "vhfev", name: "anujuwebf"}
+}
+
+//MAPS 
+const userMap = new Map<string,User1>()
+userMap.set("c",{id : "hello", name: "bjdhvbsdjs"}) 
+userMap.set("d",{id : "heldwvdlo", name: "bjdhvbdsvdsvssdjs"}) 
+
+console.log(userMap.get("c"))
+
+//EXCLUDE 
+type event = "click" | "scroll" | "mousemove"
+type excludeEvent = Exclude<event,"mousemove">
+
+function eventfn(event: excludeEvent) {
+    console.log(event)
+}
+// eventfn("mousemove")  //Not possible
+eventfn("click")  
+
+
