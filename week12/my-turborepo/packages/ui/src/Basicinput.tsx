159e869d9d8a36266bc22adb7a8c8d56bee51277
anuj solania <anujsolania0507@gmail.com>
2025-04-08 21:12:36 +0530

    understanding monorepo

---
+++ week12/my-turborepo/packages/ui/src/Basicinput.tsx
@@ -0,0 +1,10 @@
+
+export function Basicinput() {
+    return (
+        <div>
+            <input type="text" placeholder="Enter name"></input>
+            <br></br>
+            <input type="number" placeholder="enter age"></input>
+        </div>
+    )
+}
