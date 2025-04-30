77ec111a2ce911f6125f7e878cda33edf87dc2e8
anuj solania <anujsolania0507@gmail.com>
2025-04-30 20:08:04 +0530

    layout.tsx

---
+++ week14/next-app/app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={""}
+      >
+        <div className=" text-center p-4 border-b" >
+         10% off on SINUP/SIGNIN
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
